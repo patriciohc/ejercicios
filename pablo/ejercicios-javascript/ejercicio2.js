a9fc09ed81bad3b8b635acd5e8f9498842732ca1
pablo <pablorosas3115@gmail.com>
2019-06-04 18:41:35 -0500

    programa de calculo de Area

--- pablo/ejercicios-javascript/ejercicio2.js
+++ pablo/ejercicios-javascript/ejercicio2.js
@@ -1,4 +1,6 @@
 const arg = require('yargs').argv
  // Aqui accedemos a la variable “nombre” que sera pasada al ejecutar el comando 
 
- console.log("total= " +(arg.a+ arg.b))
+
+  console.log("Perimetro = " + 3.1416*(arg.radio)*2)
+  console.log("Area ="+3.1416*(arg.radio)*(arg.radio) )
