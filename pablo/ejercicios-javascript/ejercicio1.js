db5dc7ae76b8db21cb31a6a2be42084ed9889fe0
pablo <pablorosas3115@gmail.com>
2019-06-04 19:17:47 -0500

    suma de variables

--- pablo/ejercicios-javascript/ejercicio1.js
+++ pablo/ejercicios-javascript/ejercicio1.js
@@ -2,3 +2,4 @@ const arg = require('yargs').argv
  // Aqui accedemos a la variable “nombre” que sera pasada al ejecutar el comando 
 
  console.log("total= " +(arg.a+ arg.b))
+ console.log("total= " +(arg.a+ arg.b))
