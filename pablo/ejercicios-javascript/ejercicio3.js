5cbad5bfbfe07c85927d6db207921b67deb8780b
pablo <pablorosas3115@gmail.com>
2019-06-05 18:05:14 -0500

    teorema de pitagoras

--- pablo/ejercicios-javascript/ejercicio3.js
+++ pablo/ejercicios-javascript/ejercicio3.js
@@ -1,6 +1,8 @@
 const arg = require('yargs').argv
  // Aqui accedemos a la variable “nombre” que sera pasada al ejecutar el comando 
 
-
-  console.log("Perimetro = " + Math.PI*(arg.radio)*2)
-  console.log("Area ="+Math.PI*(arg.radio)*(arg.radio) )
+ 
+ 
+ console.log("hipotenusa = " +Math.sqrt(Math.pow(arg.a,2)+Math.pow(arg.b,2)))
+ 
+ 
