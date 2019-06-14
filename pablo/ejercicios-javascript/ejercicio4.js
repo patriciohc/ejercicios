b18a5ddedb942b4af368039b2f32803f499c7c7b
pablo <pablorosas3115@gmail.com>
2019-06-14 11:44:56 -0500

    calculadora

--- pablo/ejercicios-javascript/ejercicio4.js
+++ pablo/ejercicios-javascript/ejercicio4.js
@@ -2,8 +2,8 @@
 const arg = require('yargs').argv
 
 
- if(operacion="suma"){console.log("resultado= " +(arg.a + arg.b));}  
-else if(operacion="resta"){console.log("resultado= "+(arg.a - arg.b));}
- else if(operacion="multiplicacion"){ console.log("resultado= " +(arg.a * arg.b));}
-else if(operacion="division"){ console.log("resultado= " +(arg.a / arg.b));}
+ if(arg.operacion==="suma"){console.log("resultado= " +(arg.a + arg.b));}  
+else if(arg.operacion==="resta"){console.log("resultado= "+(arg.a - arg.b));}
+ else if(arg.operacion==="multiplicacion"){ console.log("resultado= " +(arg.a * arg.b));}
+else if(arg.operacion==="division"){ console.log("resultado= " +(arg.a / arg.b));}
  else{console.log("Error");}
---
+++ pablo/ejercicios-javascript/ejercicio5.js
@@ -0,0 +1,6 @@
+
+const arg = require('yargs').argv
+// Aqui accedemos a la variable “nombre” que sera pasada al ejecutar el comando 
+
+ var fechaActual= new Date();
+console.log("edad= "+ (arg.fecha) )
