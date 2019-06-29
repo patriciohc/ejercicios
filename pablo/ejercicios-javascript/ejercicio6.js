affc6f2816bce6b699cd73da8a15cd2040f9b0d2
pablo1903bin <pablorosas3115@gmail.com>
2019-06-28 19:51:39 -0500

    ejercicio6

--- pablo/ejercicios-javascript/ejercicio6.js
+++ pablo/ejercicios-javascript/ejercicio6.js
@@ -1,9 +1,9 @@
-    const arg = require('yargs').argv
-    
-     var nuevoarray=JSON.parse(arg.arreglo);
+    const arg = require("yargs").argv
+     
+    var nuevoarray=JSON.parse(arg.arreglo);
      var suma = 0;
     
-     for(var i=0 ; i<nuevoarray.length; i++)
+     for(var i=0 ;i<nuevoarray.length;i++)
              {
                 suma += nuevoarray[i];
              }
---
+++ pablo/ejercicios-javascript/ejercicio8.js
@@ -0,0 +1,31 @@
+const arg = require('yargs').argv
+    
+var nuevoarray=JSON.parse(arg.arreglo),
+suma = 0,
+max=0,
+min=0;
+for(var i=0 ; i<nuevoarray.length; i++)
+        {
+           suma += nuevoarray[i];
+        }
+for(var i=0;i<nuevoarray.length;i++)
+{
+  if(max<nuevoarray[i])
+  {
+      max=nuevoarray[i]; 
+  }
+}
+for(var i=0;i>nuevoarray.length;i++)
+{
+       if(min>nuevoarray[i])
+       {
+           min=nuevoarray[i]; 
+       }
+}
+
+console.log("Promedio= "+(suma/nuevoarray.length))
+console.log("Maximo="+max) 
+console.log("Minimo="+ min) 
+
+
+
