5cb50e2cbfae779d0060148ad7f9981edc53c234
pablo1903bin <pablorosas3115@gmail.com>
2019-07-12 10:29:46 -0500

    ordenados

--- pablo/ejercicios-javascript/ejercicio6.js
+++ pablo/ejercicios-javascript/ejercicio6.js
@@ -1,14 +1,32 @@
-const arg = require("yargs").argv
-   var nuevoarray = JSON.parse (arg.arreglo);
-      var suma = 0;
-          for ( var i = 0 ;
-                i < nuevoarray.length;
-                   i ++ ) {
-                suma += nuevoarray[i];
-           }      console.log("Promedio= "+(suma/nuevoarray.length))
-                    console.log("Maximo="+ Math.max.apply(null, nuevoarray)) 
-                       console.log("Minimo="+ Math.min.apply(null, nuevoarray)) 
-   
- 
-  
-  
+const arg = require('yargs').argv
+    var nuevoarray=JSON.parse(arg.arreglo),
+        suma = 0,
+            max = nuevoarray[0],
+               min = nuevoarray[0];
+                    for ( var i = 0; 
+                     i < nuevoarray.length;
+                       i ++
+                    )     { suma += nuevoarray[i];
+                          }
+        
+                    for ( var i = 0;
+                       i < nuevoarray.length;
+                          i ++ 
+                    ) {if ( max < nuevoarray[i] ) {
+                         max = nuevoarray[i]; 
+                    }  
+                      }     
+
+                     for ( var i = 0;
+                       i > nuevoarray.length;
+                         i ++ 
+                     ) { if ( min > nuevoarray[i] ) {
+                           min = nuevoarray[i];
+                      }            
+                }
+             console.log("Promedio= "+(suma/nuevoarray.length))
+         console.log("Maximo="+ max) 
+     console.log("Minimo="+ min) 
+
+
+
--- pablo/ejercicios-javascript/ejercicio7.js
+++ pablo/ejercicios-javascript/ejercicio7.js
@@ -1,32 +1,35 @@
-const arg = require('yargs').argv
-    var nuevoarray=JSON.parse(arg.arreglo),
-        suma = 0,
-            max = nuevoarray[0],
-               min = nuevoarray[0];
-                    for ( var i = 0; 
-                     i < nuevoarray.length;
-                       i ++
-                    )     { suma += nuevoarray[i];
-                          }
-        
-                    for ( var i = 0;
-                       i < nuevoarray.length;
-                          i ++ 
-                    ) {if ( max < nuevoarray[i] ) {
-                         max = nuevoarray[i]; 
-                    }  
-                      }     
+const arg = require('yargs').argv 
+   function  promedio (array1) {
+      var array1 = JSON.parse(arg.arreglo);
+         suma = 0;
+            for ( var i = 0; i <array1.length; i ++) {
+            suma += array1[ i ];
+         }
+    return suma/array1.length       
+   }
 
-                     for ( var i = 0;
-                       i > nuevoarray.length;
-                         i ++ 
-                     ) { if ( min > nuevoarray[i] ) {
-                           min = nuevoarray[i];
-                      }            
-                }
-             console.log("Promedio= "+(suma/nuevoarray.length))
-         console.log("Maximo="+ max) 
-     console.log("Minimo="+ min) 
+function numMaximo (array2){ 
+    var array2 = JSON.parse(arg.arreglo);
+        max = array2[0];
+           for (var x = 0; x < array2.length; x++) {
+             if ( max < array2[x] ) {
+                max = array2[x]; 
+             } 
+           }
+    return max  
+}
 
+ function numMinimo(array3) {
+     var array3 = JSON.parse(arg.arreglo);
+         min = array3 [ 0 ];
+            for ( var y = 0;  y > array3.length;  y++) {
+                if (min > array3 [ y ] ) {
+                   min = array3[y];
+                 }
+            }
+   return min
+ }
 
-
+  console.log("Promedio:"+ promedio())
+     console.log("Maximo: "+ numMaximo())
+         console.log("Minimo: "+ numMinimo())
